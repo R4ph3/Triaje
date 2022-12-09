d606ff7042086aea63f7cb83ae93485d680de2a7
Rafa Barber <96293934+R4ph3@users.noreply.github.com>
2022-12-09 14:50:45 +0100

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -2,6 +2,6 @@ import subprocess, sys
 
 print("---------BLACKLIST-------")
 p = subprocess.Popen(["powershell.exe", 
-              "C:\\Users\\rafael.barber\\Desktop\\Analizador\\blacklist.ps1"], 
+              "ruta_con_archivo_powershell_blacklist.ps1"], 
               stdout=sys.stdout)
 p.communicate()
